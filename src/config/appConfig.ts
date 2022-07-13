cc5fc11f2bf0ee1bcb15e27430f3602a6e8297e2
yazmeyaa <vip.tonuvmartini@mail.ru>
2022-07-13 09:38:35 +0300

    Added .env file support

---
+++ src/config/appConfig.ts
@@ -0,0 +1,10 @@
+const { REACT_APP_BACKEND_URL: backendUrl, NODE_ENV: nodeEnv } = process.env
+
+function getValue(value: string | undefined, defaultValue = '') {
+    return value || defaultValue
+}
+
+export const appConfig = {
+    backendUrl: getValue(backendUrl, 'localhost:3000'),
+    nodeEnv: getValue(nodeEnv, 'production'),
+}
